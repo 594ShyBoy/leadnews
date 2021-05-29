af8ab4c424ccb639073497e0158a9b51eff65823
陈晓龙 <909372455@qq.com>
2021-05-29 15:44:57 +0800

    前端

---
+++ leadnews-admin-vue/src/api/channel.js
@@ -0,0 +1,26 @@
+import Request from '@/utils/request'
+import { API_CHANNEL_LIST, API_CHANNEL_SAVE, API_CHANNEL_UPDATE } from '@/constants/api'
+
+export function loadList (data) {
+  return new Request({
+    url: API_CHANNEL_LIST,
+    method: 'post',
+    data
+  })
+}
+
+export function saveData (data) {
+  return new Request({
+    url: API_CHANNEL_SAVE,
+    method: 'post',
+    data
+  })
+}
+
+export function updateData (data) {
+  return new Request({
+    url: API_CHANNEL_UPDATE,
+    method: 'post',
+    data
+  })
+}
