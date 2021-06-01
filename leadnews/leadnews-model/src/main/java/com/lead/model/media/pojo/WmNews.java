effe5a50b8c9745fd03f1bb1a0e8ede97ff500fa
陈晓龙 <909372455@qq.com>
2021-06-01 13:32:56 +0800

    自媒体day04结束

---
+++ leadnews/leadnews-model/src/main/java/com/lead/model/media/pojo/WmNews.java
@@ -0,0 +1,19 @@
+package com.lead.model.media.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName WmNews
+ * @date 2021/6/1 13:29
+ * @Version 1.0
+ * @Author ShyBoy
+ * 自媒体图文内容信息表
+ */
+@Data
+@TableName("wm_news")
+public class WmNews implements Serializable {
+    
+}
